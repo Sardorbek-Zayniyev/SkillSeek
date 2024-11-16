bf2980af5a49bf754bebafa2e7a6f1d27d8214f2
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-11-17 01:16:36 +0500

    🔨 build: update views

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project, Review, Tag
 # Create your views here.
 
 
-
-
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = Project.objects.get(id=pk)
+    context = {
+        'project': projectObj,
+    }
+    return render(request, 'projects/single-project.html', context)
